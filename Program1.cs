6bc9aa90c14d4c575f5704213ba815c35675021c
EAgha <41126467+EAghayev@users.noreply.github.com>
2023-12-18 21:53:43 +0400

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 34;
+            int num2 = 13;
+            int num3 = 54;
+
+            int count = 0;
+
+            if(num1>0 && num1 % 2 == 0)
+            {
+                count++;
+            }
+
+            if (num2 > 0 && num2 % 2 == 0)
+            {
+                count++;
+            }
+
+            if(num3>0 && num3 % 2 == 0)
+            {
+                count++;
+            }
+
+            Console.WriteLine(count);
+
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Lesson
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int num = 1;
+            int sum = 0;
+
+            //while (num < n)
+            //{
+            //    sum += num;
+            //    num++;
+            //}
+
+            for (int i = 1; i < n; i++)
+            {
+                sum += i;
+            }
+
+
+            Console.WriteLine(sum);
+
+
+            int start = 45;
+            int end = 546;
+
+            int result = 0;
+
+            for (int i = start; i < end; i++)
+            {
+                if (i % 11 != 0)
+                {
+                    continue;
+                }
+
+                result += i;
+            }
+
+            Console.WriteLine(result);
+
+            bool check = false;
+            for (int i = start; i < end; i++)
+            {
+                if (i % 11 == 0)
+                {
+                    check = true;
+                    break;
+                }
+            }
+
+            if (check)
+            {
+                Console.WriteLine("var");
+            }
+        }
+    }
+}
